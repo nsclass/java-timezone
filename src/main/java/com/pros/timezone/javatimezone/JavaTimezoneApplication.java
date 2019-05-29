e7d362dae4c1e87661bf2150a95ff65f8e5554d0
Nam Seob Seo <nseo@pros.com>
2019-05-29 07:56:08 -0500

    refactoring.

--- src/main/java/com/pros/timezone/javatimezone/JavaTimezoneApplication.java
+++ src/main/java/com/pros/timezone/javatimezone/JavaTimezoneApplication.java
@@ -3,52 +3,139 @@ package com.pros.timezone.javatimezone;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class JavaTimezoneApplication {
 
+	private static Logger log = LoggerFactory.getLogger(JavaTimezoneApplication.class);
+
 	public static void main(String[] args) {
-
-		String[] ids = TimeZone.getAvailableIDs();
-		for (String id : ids) {
-			System.out.println(displayTimeZone(TimeZone.getTimeZone(id)));
-		}
-
-		System.out.println("\nTotal TimeZone ID " + ids.length);
-
+		buildTimezoneDisplayInfoList()
+				.forEach(System.out::println);
 	}
 
-	private static String displayTimeZone(TimeZone tz) {
-
-		TimeOffset rawOffset = buildOffset(tz.getRawOffset());
-		TimeOffset dayLightSavingOffset = buildOffset(tz.getRawOffset() + tz.getDSTSavings());
-
-		String range = daylightSavingRange(tz.getID());
-
-		StringBuilder sb = new StringBuilder();
-		sb.append("Raw: ")
-                .append(rawOffset)
-                .append(", DST: ")
-                .append(dayLightSavingOffset);
-		if (!range.isEmpty())
-		{
-			sb.append(" (")
-                    .append(range)
-                    .append(")");
-		}
-		sb.append(" ")
-                .append(tz.getID());
-
-		return sb.toString();
-	}
-
-	private static String daylightSavingRange(String tzId)
+	private static List<TimezoneDisplayInfo> buildTimezoneDisplayInfoList()
 	{
-		StringBuilder sb = new StringBuilder();
+		String[] ids = TimeZone.getAvailableIDs();
+		return Arrays.stream(ids).map(id -> createTimezoneDisplayInfo(TimeZone.getTimeZone(id)))
+				.collect(Collectors.toList());
+	}
+
+	private static class DaylightSavingItem
+	{
+		private boolean begin;
+		private String date;
+
+		public DaylightSavingItem(boolean begin, String date)
+		{
+			this.begin = begin;
+			this.date = date;
+		}
+
+		@Override
+		public String toString()
+		{
+			String str = begin ? "starting" : "ending";
+			return date + "[" + str + "]";
+		}
+	}
+
+	private static class DaylightSavingRange
+	{
+		private DaylightSavingItem first;
+		private DaylightSavingItem second;
+
+		public DaylightSavingRange(DaylightSavingItem first, DaylightSavingItem second)
+		{
+			this.first = first;
+			this.second = second;
+		}
+
+		@Override
+		public String toString()
+		{
+			return first.toString() + " - " + second.toString();
+		}
+	}
+
+	private static class TimezoneDisplayInfo
+	{
+		private String zoneId;
+		private String GMT;
+		private String DST;
+		private DaylightSavingRange daylightSavingRange;
+
+		public TimezoneDisplayInfo(String zoneId, String GMT, String DST, DaylightSavingRange daylightSavingRange)
+		{
+			this.zoneId = zoneId;
+			this.GMT = GMT;
+			this.DST = DST;
+			this.daylightSavingRange = daylightSavingRange;
+		}
+
+		@Override
+		public String toString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.append(zoneId)
+					.append(" - ")
+					.append(GMT)
+					.append(", ")
+					.append(DST);
+
+			if (daylightSavingRange != null)
+			{
+				String str = daylightSavingRange.toString();
+				if (!str.isEmpty())
+				{
+					sb.append(" (")
+							.append(str)
+							.append(")");
+				}
+			}
+			return sb.toString();
+		}
+	}
+
+	private static TimezoneDisplayInfo createTimezoneDisplayInfo(TimeZone tz)
+	{
+		String gmt = generateGMTString(tz.getRawOffset());
+		String dst = generateGMTString(tz.getRawOffset() + tz.getDSTSavings());
+
+		DaylightSavingRange daylightSavingRange = createDaylightSavingRange(tz.getID()).orElse(null);
+		return new TimezoneDisplayInfo(tz.getID(), gmt, dst, daylightSavingRange);
+	}
+
+
+	private static String generateGMTString(int offsetInMilli)
+	{
+		long hours = TimeUnit.MILLISECONDS.toHours(offsetInMilli);
+		long minutes = TimeUnit.MILLISECONDS.toMinutes(offsetInMilli)
+				- TimeUnit.HOURS.toMinutes(hours);
+		// avoid -4:-30 issue
+		minutes = Math.abs(minutes);
+
+		if (hours > 0)
+		{
+			return String.format("GMT+%d:%02d", hours, minutes);
+		}
+		else
+		{
+			return String.format("GMT%d:%02d", hours, minutes);
+		}
+	}
+
+	private static Optional<DaylightSavingRange> createDaylightSavingRange(String tzId)
+	{
 		try
 		{
 			DateTimeZone zone = DateTimeZone.forID(tzId);
@@ -58,53 +145,20 @@ public class JavaTimezoneApplication {
 			long next = zone.nextTransition(current);
 			if (current != next)
 			{
-				String str = !zone.isStandardOffset(next) ? " [starting]" : " [ending]";
-				sb.append(format.print(next) + str + " - ");
+				DaylightSavingItem first = new DaylightSavingItem(!zone.isStandardOffset(next), format.print(next));
 
 				current = next;
 				next = zone.nextTransition(current);
-				str = !zone.isStandardOffset(next) ? " [starting]" : " [ending]";
-				sb.append(format.print(next) + str);
+				DaylightSavingItem second = new DaylightSavingItem(!zone.isStandardOffset(next), format.print(next));
+
+				return Optional.of(new DaylightSavingRange(first, second));
 			}
 		}
 		catch (Exception e)
 		{
-			// System.out.println("Error: " + e.getMessage());
-		}
-		return sb.toString();
-	}
-
-
-	private static TimeOffset buildOffset(int offsetInMilli)
-	{
-		return new TimeOffset(offsetInMilli);
-	}
-
-	private static class TimeOffset
-	{
-		private long hours;
-		private long minutes;
-
-		public TimeOffset(int offsetInMilli)
-		{
-			hours = TimeUnit.MILLISECONDS.toHours(offsetInMilli);
-			minutes = TimeUnit.MILLISECONDS.toMinutes(offsetInMilli)
-					- TimeUnit.HOURS.toMinutes(hours);
-			// avoid -4:-30 issue
-			minutes = Math.abs(minutes);
+			log.error(e.getMessage(), e);
 		}
 
-		@Override
-		public String toString()
-		{
-			if (hours > 0)
-			{
-				return String.format("GMT+%d:%02d", hours, minutes);
-			}
-			else
-			{
-				return String.format("GMT%d:%02d", hours, minutes);
-			}
-		}
+		return Optional.empty();
 	}
 }
